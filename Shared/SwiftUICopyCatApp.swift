442f5cf9fd568a337d1d067314101266690aa85a
Logan Koshenka <koshenka27@gmail.com>
2021-02-11 10:57:44 -0500

    Initial Commit

---
+++ Shared/SwiftUICopyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICopyCatApp.swift
+//  Shared
+//
+//  Created by Logan  Koshenka on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICopyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
